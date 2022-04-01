7190ada76c330f5c486e42ebee6da7c1fe20ac38
jacob <jwgerlach00@gmail.com>
2022-04-01 11:07:29 -0400

    Began bert token conversion

--- bert.py
+++ bert.py
@@ -7,8 +7,8 @@ import torch
 from stse.bytes import bit_vect
 
 
-# config = 'bert-large-uncased'
-# model = BertModel.from_pretrained(config)
+config = 'bert-large-uncased'
+model = BertModel.from_pretrained(config)
 
 # print(torch.Tensor(np.array(['sdfs'])))
 
@@ -23,7 +23,7 @@ features_df = pd.read_csv('data/features.csv')
 features_df['feature_vect'] = [bit_vect(len(features_df) + 1, i) for i in range(len(features_df['feature_text']))]
 
 # APPEND AND CLEAN DATA
-# Drop blank annotations
+# Drop blank annotations ('[]')
 train_df = train_df[train_df['annotation'] != '[]']
 
 # Add features
@@ -39,20 +39,27 @@ word_lists = data['pn_history'].apply(lambda x: np.array(x.split(' '))).to_numpy
 
 # Drop and reindex any residuals
 data = data.dropna().reset_index(drop=True)
-print(data)
+
 annotations = [i.translate(i.maketrans('', '', '[]\'')).split(' ') for i in data['annotation']]
 
 assert len(annotations) == len(word_lists)
 
-print(annotations)
-print(word_lists[0])
 for note, ann in zip([word_lists[0]], [annotations[0]]):
     for word in note:
         if word in ann:
             print(word)
+            
+# Tokenize word lists
+tokenizer = BertTokenizer.from_pretrained(config)
+encoded_word_lists = [tokenizer.encode(x.tolist()) for x in word_lists]
 
 
-
+# a = torch.Tensor(encoded_word_lists)
+# print(torch.tensor(encoded_word_lists[0]))
+fits = [model(torch.tensor(np.array(x).reshape(1, -1))) for x in encoded_word_lists]
+# a = model(torch.tensor(b))  # Shape: batch_size, seq_length
+# print(a)
+print(fits)
 
 
 # annotations = [i.translate(i.maketrans('', '', '[]\'')).split(' ') for i in train_df['annotation']]
